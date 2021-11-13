15bc4b8f2d57ba4c298bb4343ebc8f2a92611fe6
João Pedro Machado <imjoaomachado@gmail.com>
2021-11-12 21:23:16 -0300

    add new website close pop-up day12

---
+++ websites/Close pop-up/main.js
@@ -0,0 +1,33 @@
+const modal = document
+                .querySelector(" .container-modal")
+
+const removeAll = document
+                .querySelector(" .container-button ")
+
+const closeModal = document
+                .querySelector(".container-modal span")
+
+const blueScreen = document
+                .querySelector(" .container-modal i ")
+
+
+
+function myModal() {
+    modal.classList.add("show-modal")
+
+}
+
+closeModal.addEventListener("click", handleClick);
+
+function handleClick() {
+    modal.classList.remove("show-modal")
+}
+
+blueScreen.addEventListener("click", addScreen)
+
+
+function addScreen() {
+    modal.classList.remove("show-modal")
+    removeAll.classList.add("remove-container-button")
+    document.body.classList.add("show-blueScreen")
+}
